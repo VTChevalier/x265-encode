b315c0644c83a7782892debedb396ad880d0f0da
VTChevalier <VTChevalier@users.noreply.github.com>
2019-04-14 18:26:34 -0500

    Added auto 2160p downscale to 1080p
    Will add option for x265 10bit HEVC @ 2160p on next iteration.

--- x264-encode.sh
+++ x264-encode.sh
@@ -9,6 +9,8 @@
 #
 # Written by Victor T. Chevalier
 #
+# Designed for use with openvpn on Ubuntu 18.04/16.04 LTS
+#
 # This script depends on two separate command line tools:
 #
 #   HandBrakeCLI    http://handbrake.fr
@@ -18,6 +20,24 @@
 #
 #   ./encode.sh [Media File].mkv/mp4/m2ts
 #
+X264JOB="/tmp/x264.job"
+createQueue() {
+  if [ -f "$X264JOB".pid ]
+  then
+    PID=`/bin/cat "$X264JOB".pid`
+  fi
+
+  echo $PID
+  while [ "$PID" != "" ] && [ -e /proc/$PID ]
+  do
+    sleep 360 # check hourly
+    if [ -f "$X264JOB".pid ]
+    then
+      PID=`/bin/cat "$X264JOB".pid`
+    fi
+  done
+}
+
 usage() { echo "Usage: $0 -q [Quick Encode] -f MEDIA.MKV" 1>&2; exit 1; }
 CURDIR=`pwd`
 
@@ -48,6 +68,50 @@ done
 if [ "$INFILE" == "0" ]; then
   usage
 elif [ ! -f "$INFILE" ]; then
+  echo -e "File not found." >&2
+  exit 1;
+fi
+
+# fine the file no mater where this is run
+DIRNAME="`/usr/bin/dirname \"$INFILE\"`"
+if [ "$DIRNAME" != "." ]; then
+  CURDIR=$DIRNAME
+fi
+
+OPTIONS="--markers --encoder x264 --encoder-tune film $TWOPASS --x264-preset $QUALITY"
+OPTIONS="$OPTIONS --encopts rc-lookahead=60:b-adapt=2:me=tesa:nal_hrd=vbr:min-keyint=1:keyint=24:bitrate=14020:vbv-maxrate=30000:vbv-bufsize=30000:ratetol=inf"
+OPTIONS="$OPTIONS --h264-profile high --h264-level 4.1"
+OPTIONS="$OPTIONS -M 709" #compatability
+OPTIONS="$OPTIONS -X 1920"
+OPTIONS="$OPTIONS --vb 14020"
+OPTIONS="$OPTIONS --crop 0:0:0:0 --auto-anamorphic"
+
+# mux HD Audio, AC3 5.1 encode second
+OPTIONS="$OPTIONS -a 1,1 -E copy,ffac3" #original audio and ac3 convert
+
+#--subtitle-forced --subtitle-default "
+OPTIONS="$OPTIONS --all-subtitles "
+
+OUTFILE="$(/usr/bin/basename \"$INFILE\" | /usr/bin/rev | /usr/bin/cut -f 2- -d '.' | /usr/bin/rev)"
+LOGFILE="/tmp/x264-$OUTFILE.log"
+INFILE="$CURDIR/`/usr/bin/basename \"$INFILE\"`"
+OUTFILE="$CURDIR/$OUTFILE-x264.mkv"
+
+COMMAND=`echo "$HANDBRAKE $OPTIONS --input "\"${INFILE}\"" --output "\"${OUTFILE}\"" > "\"${LOGFILE}\"" 2>&1"`
+
+createQueue
+
+/bin/echo $COMMAND > "$X264JOB"
+/bin/echo "rm \"$X264JOB\"" >> "$X264JOB"
+
+/bin/chmod +x "$X264JOB"
+
+/usr/bin/screen -S "Encoding $OUTPUT" -dm `/tmp/x264.job & echo $!  > "$X264JOB".pid` & 
+
+/bin/rm -f "$X264JOB".pid
+
+exit 0;
+
   echo -e "File not found." >&2
   exit 1;
 fi
