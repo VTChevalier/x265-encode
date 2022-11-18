8a9521ecfd19ae7c02a9f2a533cf2ea519db330c
VTChevalier <VTChevalier@users.noreply.github.com>
2022-11-17 21:51:45 -0600

    Update and rename x264-encode.sh to x265-encode.sh
    Changed x264 to x265

--- x265-encode.sh
+++ x265-encode.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#
-# /usr/local/bin/x264-encode.sh
+# x265-encode.sh
 #
 # Copyright (c) 2019 Free Software Foundation, Inc.
 # This is free software.  You may redistribute copies of it under the terms of
@@ -10,27 +9,27 @@
 # Written by Victor T. Chevalier
 #
 # Usage:
-#   ./x264-encode.sh [-a -q] [-f media.mkv/mp4/m2ts]
+#   ./x265-encode.sh [-a -q] [-f media.mkv/mp4/m2ts]
 #
 # Bulk files:
-#   screen -dm ./x264-encode.sh [-a -q] [-d dir]
+#   screen -dm ./x265-encode.sh [-a -q] [-d dir]
 #
 LOGDIR="/tmp/" # can set this to a specific folder if desired
 DIR=""
-X264JOB="/tmp/x264.job"
+X265JOB="/tmp/x265.job"
 createQueue() {
-  if [ -f "$X264JOB".pid ]
+  if [ -f "$X265JOB".pid ]
   then
-    PID=`/bin/cat "$X264JOB".pid`
+    PID=`/bin/cat "$X265JOB".pid`
   fi
 
   echo $PID
   while [ "$PID" != "" ] && [ -e /proc/$PID ]
   do
     sleep 360 # check hourly
-    if [ -f "$X264JOB".pid ]
+    if [ -f "$X265JOB".pid ]
     then
-      PID=`/bin/cat "$X264JOB".pid`
+      PID=`/bin/cat "$X265JOB".pid`
     fi
   done
 }
@@ -83,7 +82,7 @@ if [ "$DIR" != "" ]; then
         PASSARGS="-q $PASSARGS"
       fi
 
-      `./x264-encode.sh $PASSARGS -f "$dir"`
+      `./x265-encode.sh $PASSARGS -f "$dir"`
     fi
   done
 fi
@@ -101,16 +100,16 @@ if [ "$DIRNAME" != "." ]; then
   CURDIR=$DIRNAME
 fi
 
-OPTIONS="--markers --encoder x264 --encoder-tune $TUNE $TWOPASS --x264-preset $QUALITY"
+OPTIONS="--markers --encoder x265_10bit --encoder-tune $TUNE $TWOPASS --x265-preset $QUALITY"
 OPTIONS="$OPTIONS -X 1920 -Y 1080"
 OPTIONS="$OPTIONS --crop 0:0:0:0 --auto-anamorphic"
 
 # mux HD Audio, AC3 5.1 encode second
-OPTIONS="$OPTIONS --audio 1,2,3,4,5,6,7,8,9,10,11 --aencoder copy,copy,copy,copy,copy,copy,copy,copy,copy,copy,copy --audio-copy-mask ac3,eac3,dts,dtshd,truehd"
+OPTIONS="$OPTIONS --audio 1,2,3,4,5,6,7,8,9,10,11 --aencoder copy,copy,copy,copy,copy,copy,copy,copy,copy,copy,copy --audio-copy-mask ac3,eac3,dts,dtshd"
 
-# x264 codec specifics
+# x265 codec specifics
 OPTIONS="$OPTIONS --encopts rc-lookahead=60:b-adapt=2:me=tesa:nal_hrd=vbr:min-keyint=1:keyint=24:bitrate=$BITRATE:vbv-maxrate=30000:vbv-bufsize=30000:ratetol=1.0"
-OPTIONS="$OPTIONS --h264-profile high --h264-level 4.1"
+OPTIONS="$OPTIONS --encoder-profile main10"
 OPTIONS="$OPTIONS --vb $BITRATE"
 
 # this preserves subtitles
@@ -119,22 +118,22 @@ OPTIONS="$OPTIONS --all-subtitles"
 
 INFILE="`/usr/bin/basename \"$INFILE\"`"
 OUTFILE="$(echo ${INFILE} | /usr/bin/rev | /usr/bin/cut -f 2- -d '.' | /usr/bin/rev)"
-LOGFILE="${LOGDIR}x264-$OUTFILE.log"
+LOGFILE="${LOGDIR}x265-$OUTFILE.log"
 INFILE="$CURDIR/${INFILE}"
-OUTFILE="$CURDIR/$OUTFILE-x264.mkv"
+OUTFILE="$CURDIR/$OUTFILE-x265.mkv"
 
 COMMAND=`echo "$HANDBRAKE $OPTIONS --input "\"${INFILE}\"" --output "\"${OUTFILE}\"" > "\"${LOGFILE}\"" 2>&1"`
 
 createQueue
 
-/bin/echo $COMMAND > "$X264JOB"
-#/bin/echo "/bin/rm \"$X264JOB\"" >> "$X264JOB"
-#/bin/echo "/bin/rm \"$X264JOB\".pid" >> "$X264JOB"
-/bin/echo "exit" >> "$X264JOB"
+/bin/echo $COMMAND > "$X265JOB"
+#/bin/echo "/bin/rm \"$X265JOB\"" >> "$X265JOB"
+#/bin/echo "/bin/rm \"$X265JOB\".pid" >> "$X265JOB"
+/bin/echo "exit" >> "$X265JOB"
 
-/bin/chmod +x "$X264JOB"
+/bin/chmod +x "$X265JOB"
 
-/usr/bin/screen -S "x264 encoding" -dm `/tmp/x264.job & echo $!  > "$X264JOB".pid` & 
+/usr/bin/screen -S "x265 encoding" -dm `/tmp/x265.job & echo $!  > "$X265JOB".pid` & 
 
 
 exit 0;
